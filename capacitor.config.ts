45c6fc882f185d41a1156472d6d9fa38af196c5d
Leonid <87147027+leo-mv@users.noreply.github.com>
2024-11-15 14:39:17 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'podium',
+  webDir: 'dist'
+};
+
+export default config;
